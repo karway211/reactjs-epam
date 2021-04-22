340c663e7856d84c68f7265d822fe9595a3248d7
Andrey Karchevskiy <karway211@gmail.com>
2021-04-22 10:51:17 +0300

    Revert "react-task-1 feat(*): add solution task"

--- react-epam/src/pages/FooterContainer/helpers.ts
+++
@@ -1,7 +0,0 @@
-export const getPhoneMask = (phone: string): string => {
-  const x = phone
-    .replace(/\D/g, '')
-    .slice(-9)
-    .match(/(\d{2})(\d{3})(\d{2})(\d{2})/);
-  return `8-0${x![1]}-${x![2]}-${x![3]}-${x![4]}`;
-}
